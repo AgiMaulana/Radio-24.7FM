685f76c9d8193e6d53aa598b6821f9bf7c5f6410
AgiMaulana <agimaulana.dex@gmail.com>
2026-05-28 23:51:17 +0700

    feat(widget): refactor RadioWidget to use SmallWidget composable
    Replace manual layout in RadioWidget with the SmallWidget composable, passing ic_star_filled as the title icon for a simpler widget-appropriate appearance.

--- feature/widget/src/main/kotlin/io/github/agimaulana/radio/feature/widget/RadioWidget.kt
+++ feature/widget/src/main/kotlin/io/github/agimaulana/radio/feature/widget/RadioWidget.kt
@@ -2,35 +2,18 @@ package io.github.agimaulana.radio.feature.widget
 
 import android.content.Context
 import androidx.glance.GlanceId
-import androidx.glance.GlanceModifier
+import androidx.glance.ImageProvider
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
-import androidx.glance.background
-import androidx.glance.layout.Alignment
-import androidx.glance.layout.Column
-import androidx.glance.layout.fillMaxSize
-import androidx.glance.text.Text
-import androidx.glance.text.TextStyle
-import androidx.glance.unit.ColorProvider
+import io.github.agimaulana.radio.feature.widget.small.SmallWidget
 
 class RadioWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            Column(
-                modifier = GlanceModifier
-                    .fillMaxSize()
-                    .background(android.R.color.black),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(
-                    text = "247FM",
-                    style = TextStyle(
-                        color = ColorProvider(android.R.color.white),
-                    ),
-                )
-            }
+            SmallWidget(
+                titleIcon = ImageProvider(R.drawable.ic_star_filled),
+            )
         }
     }
 }
